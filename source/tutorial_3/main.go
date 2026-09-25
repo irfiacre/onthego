ba8aa3bebfbe22f8685c2ac2deb1cafbd6b23b0c
irfiacre <irfiacre@gmail.com>
2026-09-25 17:10:35 +0200

    Improvements

---
+++ source/tutorial_3/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arrays, Slices, Maps, Loops
+	// var intArr [3]int32 //Array that can hold 3 integers of type int32. Default types here is [0,0,0]
+
+	// fmt.Println(intArr[0], intArr[1:3]) //indexability
+
+	// // Memory of each element
+	// fmt.Println(&intArr[0])
+	// fmt.Println(&intArr[1])
+	// fmt.Println(&intArr[2])
+
+	// Initializing the array
+	var intArr2 [3]int = [3]int{1, 2, 3} // or use: var intArr2 := [3]int{1, 2, 3}
+	fmt.Println(intArr2)
+
+	// Slices
+
+}
